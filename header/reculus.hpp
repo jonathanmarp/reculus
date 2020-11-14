788c3e7006136ea84f1b2cfe29e5b3658a95dce9
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-14 20:16:20 +0700

    Update reculus.hpp

--- header/reculus.hpp
+++ header/reculus.hpp
@@ -1,3 +1,8 @@
+// This File From ../reculus.cpp
+// map the script for simple and short to look
+// From jonathan for reculus Database Memory
+// Copyright 2020 - 2025
+
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 

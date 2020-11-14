026ea97167d5d6122a5818f600b636951022e2d0
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-14 20:12:52 +0700

    Update reculus.cpp

--- reculus.cpp
+++ reculus.cpp
@@ -2,21 +2,28 @@
 // This First Play In App
 // Server side C/C++ program to demonstrate Socket programming 
 // Copyright 2020 - 2025
+// This Server For Database Memory
+
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
+#include <string>
+#include <cstring>
+#include <iostream>
+
+#define PORT 6045
+
 int main(int argc, char const *argv[]) 
 { 
+	std::cout << "Reculus In Start Now" << std::endl;
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+    const char* hello = "Hello from server"; 
        
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
@@ -25,7 +32,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE); 
     } 
        
-    // Forcefully attaching socket to the port 8080 
+    // Forcefully attaching socket to the port 6045 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                   &opt, sizeof(opt))) 
     { 
@@ -35,8 +42,9 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
+	std::cout << "reculus start in port : " << 6045 << std::endl;
        
-    // Forcefully attaching socket to the port 8080 
+    // Forcefully attaching socket to the port 6045 
     if (bind(server_fd, (struct sockaddr *)&address,  
                                  sizeof(address))<0) 
     { 
@@ -53,10 +61,14 @@ int main(int argc, char const *argv[])
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
     valread = read( new_socket , buffer, 1024); 
     printf("%s\n",buffer ); 
     send(new_socket , hello , strlen(hello) , 0 ); 
     printf("Hello message sent\n"); 
+	{
+		(void)argc;
+		(void)argv;
+	}
     return 0; 
 }

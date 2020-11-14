a2a443b3f4752b8d815a2c4cffed8f4f139982b9
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-14 20:17:13 +0700

    Update config.hpp

--- header/config.hpp
+++ header/config.hpp
@@ -1,1 +1,11 @@
+// This File From ../reculus.cpp
+// map the script for simple and short to look
+// From jonathan for reculus Database Memory
+// Copyright 2020 - 2025
+
+#ifndef __CONFIG
+#define __CONFIG
+
 #define PORT 6045
+
+#endif

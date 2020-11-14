7b4969ca987b47d4140653bc8a9deb6e7773c127
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-14 19:59:03 +0700

    Test The First Script
    Testing The Script Is Ready

---
+++ reculus.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+  cout << "Hello, World" << endl;
+  {
+    (void)argc;
+    (void)argv;
+  }
+  return 0;
+}

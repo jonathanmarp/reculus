65611ca4aabcc3da487663f7fd4a74c6d7f25d60
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-15 08:56:33 +0700

    Update reculus.cpp

--- client/reculus.cpp
+++ client/reculus.cpp
@@ -2,42 +2,66 @@
 // This First Play In App
 // Server side C/C++ program to demonstrate Socket programming 
 // Copyright 2020 - 2025
-// This Server For Database Memorylient side C/C++ program to demonstrate Socket programming
+// This Server For Database Memory
 
 #include "../header/reculus.hpp"
 #include "../header/config.hpp"
 
 int main(int argc, char const *argv[]) 
 { 
-	std::cout << "Reculus{CLIENT} : Start Now" << std::endl;
-	int sock = 0, valread; 
-	struct sockaddr_in serv_addr; 
-	const char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	{ 
-		printf("\n Socket creation error \n"); 
-		return -1; 
-	} 
-	std::cout << "Reculus{CLIENT} : Secure Run" << std::endl;
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
-	
-	// Convert IPv4 and IPv6 addresses from text to binary form 
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-	{ 
-		printf("\nInvalid address/ Address not supported \n"); 
-		return -1; 
-	} 
+	std::cout << "Reculus In Start Now" << std::endl;
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address;
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    const char* hello = "Hello from server"; 
+    std::cout << "reculus start in port : " << 6045 << std::endl;
+    while(true) {
+		// Creating socket file descriptor 
+		if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+		{ 
+			perror("socket failed"); 
+			exit(EXIT_FAILURE); 
+		} 
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-	{ 
-		printf("\nConnection Failed \n"); 
-		return -1; 
-	} 
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	valread = read( sock , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	return 0; 
+		// Forcefully attaching socket to the port 6045 
+		if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+													  &opt, sizeof(opt))) 
+		{ 
+			perror("setsockopt"); 
+			exit(EXIT_FAILURE); 
+		} 
+		address.sin_family = AF_INET; 
+		address.sin_addr.s_addr = INADDR_ANY; 
+		address.sin_port = htons( PORT ); 
+
+		// Forcefully attaching socket to the port 6045 
+		if (bind(server_fd, (struct sockaddr *)&address,  
+									 sizeof(address))<0) 
+		{ 
+			perror("bind failed"); 
+			exit(EXIT_FAILURE); 
+		} 
+		if (listen(server_fd, 3) < 0) 
+		{ 
+			perror("listen"); 
+			exit(EXIT_FAILURE); 
+		} 
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+						   (socklen_t*)&addrlen))<0) 
+		{ 
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		}
+		valread = read( new_socket , buffer, 1024); 
+		printf("%s\n",buffer ); 
+		send(new_socket , hello , strlen(hello) , 0 ); 
+		printf("Hello message sent\n");
+	}
+	{
+		(void)argc;
+		(void)argv;
+	}
+    return 0; 
 }
